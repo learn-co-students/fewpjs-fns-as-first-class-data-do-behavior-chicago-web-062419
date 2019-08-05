1e079d85a8bdc893664a9309342e1a302c2b8d02
Adrian M Popan <adrian.popan@gmail.com>
2019-08-05 14:41:25 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,26 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+var today = new Date();
+var time = today.getHours();
+
+function greet(time) {
+  time = parseInt(time, 10)
+  if ( time < 12 ) {
+    return 'Good Morning';
+  } else if ( time > 17 ) {
+    return 'Good Evening';
+  } else {
+    return 'Good Afternoon';
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  let content = document.getElementById("greeting");
+  content = content.innerText = msg;
+}
+
+ 
